4c0dacfca4ae1251d6002ec77027ecfb0617a8b7
Amy Hurford <ahurford@mun.ca>
2019-09-10 14:56:38 -0230

    Update IntroToR.R

--- Labs/Lab 1/IntroToR.R
+++ Labs/Lab 1/IntroToR.R
@@ -1,39 +1,103 @@
 #SL:10/01/2014 An introduction to R
 #Created by Shawn J. Leroux & Amy Hurford  sleroux@mun.ca  ahurford@mun.ca
+#Revised by AH on 09/10/2019
 #Good resources for R basics: http://www.statmethods.net/   www.r4all.org   http://brianmcgill.org (notes tab)
 
-#First, clear all the old stuff in R. Always put this at the top of your code.
-rm(list=ls())
+#In class you will be running scripts that are working or mostly working. However,
+#to understand the code, and as a step toward eventually learning to program yourself
+#it is helpful practice running some commands yourself.
 
-#Some basic things to remember
-# 1) anything that comes after a # is a comment that will not be computed by R. This is a good way to insert comments in your code. For assignments that require you to hand in code, we expect you to comment your code so that we can follow your procedures - ie make your code and results easily reproducible.
-# 2) R is case sensitive. If your code is not running first check to see if there are any spelling mistakes or case mistakes.
-# 3) R has a huge user community. You can most likely find solutions to any problems you have by typing your question about R in google. We expect you to first try to solve your own problems by troubleshooting using google or R help (help()). 
-# 4) R has a large number of built-in functions. For example read.csv is a function to read .csv files into R. Sometimes you will have to download R packages to access additional functions. For example the package 'lattice' has nice functions to create 3D plots. To download this package you can use the function install.packages() or you can use the menu 'Packages & Data' - Get list and scroll to the 'lattice' package. Once you have downloaded the package, to use it you must open the package by typing 'library(lattice)'. It is useful to list the libraries you plan to use in your code at the top of your code file.
-# 5) Always type your code in some kind of text editor like Rstudio or notepad. R for Mac has a decent built-in text editor. This will allow you to reproduce your analysis easily.
-# 6) To run your code you can a) copy and paste from your text editor to the R console, b) highlight your code and type apple-return on macs or shift-return on pc
-# 7) you can create an object named pretty much anything - a, b, HgW, HJ6, Data1, etc. R will overwrite values of your objects as you use them. So if you want to use an object throughout, it is best not to name any new objects with the same name. ie only have one object named a, one named b, etc.
+#To start make sure you know:
+# -the difference between R and R studio.
+# -the purpose of the 'Editor' and of the 'Console'
+# -the difference between 'Source' and 'Run'
+# -how to execute a Source command either with the button or via a console
+# -the lines beginning with # are comments. The other black/blue lines are actual
+# commands.
+# -'Source' will run the script with each of the lines run sequentinally.
+
+# 1. R remembers only the last value of a parameter, and it remembers that
+# until you clear it.
+#####
+# Run these two lines only:
+r<-2
+r<-5000
+# type 'r' into the console, to learn what is the value of r. What is r?
+# Now run this line:
+r<-4
+# ... What is the value of 'r' now? despite 'r' having had 3 different values,
+# the current value of 'r' is the last value of 'r' and old values will not be
+# remembered unless you use a different procedure.
+
+# Because R remembers, it's a good idea to clean out old values before
+# starting:
+rm(list=ls())
+# now what is the value of 'r'?
 
 #At its simplest R is a calculator
+# (run the lines below and subsequently step through this code by reading
+# the comments and running the blue/black code lines)
 2+2
 2*4
 1/4
 
-#If you have data, you can import it into R and run calculations on your data. We will import a sample of the data used in Leroux & Kerr 2013 Land Development in and around protected areas at the wilderness frontier. Conservation Biology 27:166-176.
+# 2. Loading in data
+########
+#If you have data, you can import it into R and run calculations on your data.
+#We will import a sample of the data used in Leroux & Kerr 2013 Land Development
+# in and around protected areas at the wilderness frontier. Conservation Biology
+# 27:166-176.
+
 MyData <-read.csv("PA_Data.csv") 
 
 #Check to see if your data are correct by visualizing it in R
 #Note the type of data you have; numeric, integer, factor, etc.
 str(MyData)
 
-#In the above expression -str- is a function and it is applied to the object in parenthesis - the data
+# read.csv() is a built in R function. This means that it is a function and we
+# didn't make it up it was already there. If you would like to understand how
+# the read.csv function works try:
+?read.csv
+??read.csv
+# or you could even use a search engine and type in 'read.csv AND R'.
 
-#This outputs the first 6 lines of your data
+#In the above expression str() is another built-in function
+# and it is applied to the object in parenthesis - the data, to reveal its
+# STRucture.
+
+#This outputs the first 6 lines of your data. head is another built in function.
 head(MyData)
 
-#...and the last 6 lines of your data
+# What if we try this?
+headache(MyData)
+# ... we get an error because unlike str() and head(), there is no built-in
+# function called headache. The error message gives us a clue about what the
+# problem is. Whenever you are stuck and your code isn't working a good idea
+# is to copy your error message into a search engine - it's common to find that
+# others may have already had and solved the same problem you are having.
+
+#... on the otherhand tail() is a built-in function that shows the last 6 lines
+# of your data
 tail(MyData)
 
+# What about this?
+tail(MyNewData)
+# ... nice try, but you've not defined anything named 'MyNewData'. On what line
+# of this script did you define 'MyData'?
+
+# 3. Getting the path to a file
+#######
+# 'PA_Data.csv' loaded in with just the file name, but if you'd saved 'PA_Data.csv'
+# in another folder, this might not have been so easy.
+getwd()
+# This will tell you the path to 'IntroToR.R'. Using this as a guide you can
+# probably figure out the path to fine 'PA_Data.csv'. To test out this method
+# try to load in 'PA_Data.csv' using the comelte path like this:
+MyNewData <-read.csv("/Users/amyhurford/Desktop/BIOL-3295/Labs/Lab 1/PA_Data.csv") 
+# Did it work? What commands might you write to check that MyNewData loading in ok?
+
+# 4. Assigning values to x and y.
+#####
 #You can generate data or numbers in many ways in R
 #Create a sequence of numbers from 1 to 100 by 1
 seq(1:100)
@@ -41,28 +105,53 @@ seq(1:100)
 #Or have it increment by 0.5 instead
 seq(1,100,0.5)
 
+#If you're wondering what are appropriate arguments of seq(), i.e.,
+#what is okay to put inside the (), you might try this:
+?seq
+
 #You can store this sequence as a vector by creating an object named x that stores the sequence
 x <- seq(1:100)
 
 #Type x to see that you created this vector correctly.
 x
+# if you just do seq(1:100) but don't give it a name (e.g. 'x'), then R won't remember it.
 
 #Take median of x
 median(x)
+#... median() is another example of a built-in function that comes with the standard
+# R installation.
 
 #You can generate random numbers in R. runif() is the function to generate random numbers from a uniform distribution
 #How about 100 random numbers drawn from a uniform distribution with minimum = 0 and maximum = 10
 y <- runif(100, min = 0, max = 10)
 y
 
-
-########
-# PLOTS
+# 5. Let's make a plot
 ########
 #Now, we have two objects with length 100. We can create a plot of these data.
-#Intuitively, the primary function for plotting is called 'plot'. Let's see what arguments 'plot' takes so we know how to make our figure of x vs y
+#Intuitively, the primary function for plotting is called 'plot'.
+#Let's see what arguments 'plot' takes so we know how to make our figure of x vs y
+
 help(plot)
-#Type help(par) to find out everything you can do with this simple plotting function. par describes the different arguments you can provide to plot to change the appearance of your plot.
+
+# What if I try to make a plot and the 'x' and 'y' arguments have different lengths?
+plot(y,1)
+# opps, that's an error:
+length(1)
+length(y)
+# y has length 100 but 1 has length only equal to 1. Let's try it like this:
+plot(y,rep(1,100))
+# okay, that worked, but what is rep(1,100)? Try below and see
+rep(1,100)
+# and what is it's length?
+length(rep(1,100))
+length(y)
+# okay, plot(y,rep(1,100)) is a working command because both the x and y arguments
+# have length 100.
+
+#Type help(par) to find out everything you can do with this simple plotting function.
+# par describes the different arguments you can provide to plot to change
+# the appearance of your plot.
 plot(x,y)
 #Your plot would look better if you added some labels and a plot title, increase data point size and font size
 plot(x,y,main="My Figure", xlab="Sequence", ylab="Random Numbers", cex=1.5, cex.lab=1.5)
@@ -102,20 +191,49 @@ plot(x,y,main="My Figure", xlab="Sequence", ylab="Random Numbers", ylim=c(0,12),
 points(x,w, cex=1.5, col="red")
 legend("topleft", legend=c("Random Numbers 1", "Random Numbers 2"), pch=21, pt.bg=c("Blue", "Red"), pt.cex=1.5)
 
+# 6. Working with dataframes
 #############
-# DATA FRAMES
-#############
-#Being able to create data frames or tables is a fundamental skill you should practice
-#Data frames are the best way to store data in R. It is easy to work with (ie run analyses) on data frames
-#Create a data frame called "DataData". The data frame will have three columns named -> Column1, Column2, and Column3. Each column will have 4 rows of data.
-DataData <- data.frame(Column1=c(2,4,6,8), Column2=seq(1:4), Column3=c(8,6,4,2))
+# If you want to make a list use the built-in function c()
+c(2,4,6,8)
+
+# If you want to make a list of words (called 'strings') you need to use ''
+c('cold','warm','hot')
+
+# What if we do:
+c(cold,warm,hot)
+#... that doesn't work because R is looking for objects named cold, warm, and hot
+# Try this:
+cold<-1
+warm<-2
+hot<-1000
+c(cold,warm,hot)
+# The '' are needed so that R knows there are words and not the names of objects
+# or variables.
+
+# A dataframe is a list of lists with names at the top to help us remember
+# what each column means
+DataData <- data.frame(Monday=c(2,4,6,8), Tuesday=seq(1:4), Wednesday=c(8,6,4,2))
 DataData
-#you can look up what the function c() does
-#Now, let's add a column named 'Column4' to the data frame. You can call columns from data frames by listing them after a $ as below.
-DataData$Column4 <- c(0.1,0.2,0.3,0.4)
+#... in the case of the data.frame function we didn't need the '' on the names
+# of the columns, because these are already assumed to be strings for that specific
+# function.
+
+# What might happen if the columns don't have the same length?
+Data2 <- data.frame(Monday=1, Tuesday=2, Wednesday=c(1,2,3))
+Data2
+# Surprisingly that did work!
+Data3 <- data.frame(Monday=c(1,2), Tuesday=2, Wednesday=c(1,2,3))
+Data3
+# ... not so lucky this time!
+
+#Now, let's add a column named 'Thursday' to the data frame. You can call
+# columns from data frames by listing them after a $ as below.
+DataData$Thursday <- c(0.1,0.2,0.3,0.4)
 DataData
-#Let's multiply Column1 x Column4 and put the results in a new column called Column5
-DataData$Column5 <- DataData$Column1 * DataData$Column4
+
+# What is the point of all this? Sometimes you might load in data as a .csv file
+# as we did above with
+
 
 ###########
 # FOR LOOPS
@@ -250,5 +368,12 @@ par(mfrow=c(1,1))
 plot(x,g, xlab="x", ylab="g(x)", type="l", cex.lab=1.5, cex.axis=1.5, lwd=2)
 #Add your approximation lines here by using the 'lines' function
 #lines(x,g0,lty="dashed", lwd=2)
-
+#Some basic things to remember
+# 1) anything that comes after a # is a comment that will not be computed by R. This is a good way to insert comments in your code. For assignments that require you to hand in code, we expect you to comment your code so that we can follow your procedures - ie make your code and results easily reproducible.
+# 2) R is case sensitive. If your code is not running first check to see if there are any spelling mistakes or case mistakes.
+# 3) R has a huge user community. You can most likely find solutions to any problems you have by typing your question about R in google. We expect you to first try to solve your own problems by troubleshooting using google or R help (help() or ?topic or ??topic where topic is what you want to learn about). 
+# 4) R has a large number of built-in functions. For example read.csv is a function to read .csv files into R. Sometimes you will have to download R packages to access additional functions. For example the package 'lattice' has nice functions to create 3D plots. To download this package you can use the function install.packages() or you can use the menu 'Packages & Data' - Get list and scroll to the 'lattice' package. Once you have downloaded the package, to use it you must open the package by typing 'library(lattice)'. It is useful to list the libraries you plan to use in your code at the top of your code file.
+# 5) Always type your code in some kind of text editor like Rstudio or notepad. R for Mac has a decent built-in text editor. This will allow you to reproduce your analysis easily.
+# 6) To run your code you can a) copy and paste from your text editor to the R console, b) highlight your code and type apple-return on macs or shift-return on pc
+# 7) you can create an object named pretty much anything - a, b, HgW, HJ6, Data1, etc. R will overwrite values of your objects as you use them. So if you want to use an object throughout, it is best not to name any new objects with the same name. ie only have one object named a, one named b, etc.
 
